267f5c4cd204e8b0a3f246b67d79c77f932436e8
Victor Hugo <hugoowvicttor2006@gmail.com>
2026-08-17 22:23:30 -0300

    criando projeto

---
+++ SistemaOficina/src/test/java/org/example/sistemaoficina/SistemaOficinaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sistemaoficina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaOficinaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
