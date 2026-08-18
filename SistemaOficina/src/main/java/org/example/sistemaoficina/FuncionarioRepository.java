c33ad55b09d490d515c169740e9839b1e8203d1d
Victor Hugo <hugoowvicttor2006@gmail.com>
2026-08-17 22:25:33 -0300

    criando repositorio para funcionario

---
+++ SistemaOficina/src/main/java/org/example/sistemaoficina/FuncionarioRepository.java
@@ -0,0 +1,9 @@
+package org.example.sistemaoficina;
+
+import org.example.sistemaoficina.entity.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+}
